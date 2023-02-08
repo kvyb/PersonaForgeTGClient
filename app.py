7a48887f5cbda76b87da2ce9554ca969bccdae66
kvyb <vybiral.kristian@gmail.com>
2023-02-08 22:02:13 +0200

    The real initial commit

--- app.py
+++ app.py
@@ -1,236 +1,177 @@
-import time
-import re
-import pyfiglet
+"""
+First, a few callback functions are defined. Then, those functions are passed to
+the Application and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+# TODO Add "Typing" UI effect before response. See other Github for this.
+import websockets
 import logging
-import logging.config
 import os
-import requests
+import pyfiglet
+
+from telegram import __version__ as TG_VER
+
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 5):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Starts the conversation and asks the user about their gender."""
+    reply_keyboard = [["Boy", "Girl", "Other"]]
+
+    await update.message.reply_text(
+        "Hi! My name is Professor Bot. I will hold a conversation with you. "
+        "Send /cancel to stop talking to me.\n\n"
+        "Are you a boy or a girl?",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
+        ),
+    )
+
+    return GENDER
+
+
+async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the selected gender and asks for a photo."""
+    user = update.message.from_user
+    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    await update.message.reply_text(
+        "I see! Please send me a photo of yourself, "
+        "so I know what you look like, or send /skip if you don't want to.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PHOTO
+
+
+async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the photo and asks for a location."""
+    user = update.message.from_user
+    photo_file = await update.message.photo[-1].get_file()
+    await photo_file.download_to_drive("user_photo.jpg")
+    logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
+    await update.message.reply_text(
+        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
+    )
+
+    return LOCATION
+
+
+async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Skips the photo and asks for a location."""
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    await update.message.reply_text(
+        "I bet you look great! Now, send me your location please, or send /skip."
+    )
+
+    return LOCATION
+
+
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the location and asks for some info about the user."""
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    await update.message.reply_text(
+        "Maybe I can visit you sometime! At last, tell me something about yourself."
+    )
+
+    return BIO
+
+
+async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Skips the location and asks for info about the user."""
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    await update.message.reply_text(
+        "You seem a bit paranoid! At last, tell me something about yourself."
+    )
 
+    return BIO
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-import telegram
 
-from _model import *
+async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the info about the user and ends the conversation."""
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    await update.message.reply_text("Thank you! I hope we can talk again some day.")
 
+    return ConversationHandler.END
 
-def get_chat_id(update, context):
-    chat_id = -1
 
-    if update.message is not None:
-        chat_id = update.message.chat.id
-    elif update.callback_query is not None:
-        chat_id = update.callback_query.message.chat.id
-    elif update.poll is not None:
-        chat_id = context.bot_data[update.poll.id]
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
 
-    return chat_id
+    return ConversationHandler.END
 
 
-def get_user(update):
-    user: User = None
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("6038194661:AAF3UGR6OHNnIHPd64mw7fYYpZJKo94WVMM").build()
 
-    _from = None
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), gender)],
+            PHOTO: [MessageHandler(filters.PHOTO, photo), CommandHandler("skip", skip_photo)],
+            LOCATION: [
+                MessageHandler(filters.LOCATION, location),
+                CommandHandler("skip", skip_location),
+            ],
+            BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
 
-    if update.message is not None:
-        _from = update.message.from_user
-    elif update.callback_query is not None:
-        _from = update.callback_query.from_user
+    application.add_handler(conv_handler)
 
-    if _from is not None:
-        user = User()
-        user.id = _from.id
-        user.first_name = _from.first_name if _from.first_name is not None else ''
-        user.last_name = _from.last_name if _from.last_name is not None else ''
-        user.lang = _from.language_code if _from.language_code is not None else 'n/a'
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling()
 
-    logging.info(f'from {user}')
 
-    return user
-
-
-def start_command_handler(update, context):
-    """Send a message when the command /start is issued."""
-    add_typing(update, context)
-    buttons = MultiItems("What would you like to receive?", ["Text", "File", "GoogleDoc", "Gallery"])
-    add_suggested_actions(update, context, buttons)
-
-
-def help_command_handler(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Type /start')
-
-
-def main_handler(update, context):
-    logging.info(f'update : {update}')
-
-    if update.message is not None:
-        user_input = get_text_from_message(update)
-        logging.info(f'user_input : {user_input}')
-
-        # reply
-        add_typing(update, context)
-        add_text_message(update, context, f"You said: {user_input}")
-
-    elif update.callback_query is not None:
-        user_input = get_text_from_callback(update)
-        logging.info(f'user_input : {user_input}')
-
-        if user_input == 'Simple Text':
-            add_typing(update, context)
-            add_text_message(update, context, "Hello from the Bot 😎")
-        elif user_input == 'File':
-            # file from Github
-            url = "https://github.com/gcatanese/TelegramBotDemo/raw/main/files/test.pdf"
-            add_document(update, context, url)
-        elif user_input == 'GoogleDoc':
-            # google doc
-            url = "https://docs.google.com/document/d/10KPUejkqitf2lKHzNUreSO5TwlQ3XDwi_mb-CiAD8Zg/edit?usp=sharing"
-            fetch_and_send(update, context, url)
-        elif user_input == 'Gallery':
-            send_gallery(update, context)
-
-
-def add_typing(update, context):
-    context.bot.send_chat_action(chat_id=get_chat_id(update, context), action=telegram.ChatAction.TYPING, timeout=1)
-    time.sleep(1)
-
-
-def add_text_message(update, context, message):
-    if update.message is not None:
-        #context.bot.send_message(chat_id=get_chat_id(update, context), text=message)
-        update.message.reply_text(message)
-    elif update.callback_query is not None:
-        update.callback_query.message.edit_text(message)
-
-
-def add_suggested_actions(update, context, response):
-    options = []
-
-    for item in response.items:
-        options.append(InlineKeyboardButton(item, callback_data=item))
-
-    reply_markup = InlineKeyboardMarkup([options])
-
-    # update.message.reply_text(response.message, reply_markup=reply_markup)
-    context.bot.send_message(chat_id=get_chat_id(update, context), text=response.message, reply_markup=reply_markup)
-
-
-def add_document(update, context, url):
-    context.bot.send_document(chat_id=get_chat_id(update, context), document=url)
-
-
-def fetch_and_send(update, context, url):
-    r = requests.get(url, allow_redirects=True)
-    print(r)
-    open('googledoc.docx', 'wb').write(r.content)
-
-    context.bot.send_document(chat_id=get_chat_id(update, context), document=open('googledoc.docx', 'rb'), filename="googledoc.docx")
-
-
-def send_gallery(update, context):
-    list = []
-
-    list.append(InputMediaPhoto(media='https://github.com/gcatanese/TelegramBotDemo/raw/main/files/mintie.jpg', caption='Mint'))
-    list.append(InputMediaPhoto(media='https://github.com/gcatanese/TelegramBotDemo/raw/main/files/pinkie.jpg', caption='Pink'))
-    list.append(InputMediaPhoto(media='https://github.com/gcatanese/TelegramBotDemo/raw/main/files/orangie.jpg', caption='Orange'))
-
-    context.bot.send_media_group(chat_id=get_chat_id(update, context), media=list)
-
-
-def get_text_from_message(update):
-    return update.message.text
-
-
-def get_text_from_callback(update):
-    return update.callback_query.data
-
-
-def extract_hashtags(text):
-    return re.findall(r"#(\w+)", text)
-
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logging.warning('Update "%s" ', update)
-    logging.exception(context.error)
-
-
-def get_user(update):
-    user: User = None
-
-    _from = None
-
-    if update.message is not None:
-        _from = update.message.from_user
-    elif update.callback_query is not None:
-        _from = update.callback_query.from_user
-
-    if _from is not None:
-        user = User()
-        user.id = _from.id
-        user.first_name = _from.first_name if _from.first_name is not None else ''
-        user.last_name = _from.last_name if _from.last_name is not None else ''
-        user.lang = _from.language_code if _from.language_code is not None else 'n/a'
-
-    return user
-
-
-def main():
-    updater = Updater(DefaultConfig.TELEGRAM_TOKEN, use_context=True)
-
-    dp = updater.dispatcher
-
-    # command handlers
-    dp.add_handler(CommandHandler("help", help_command_handler))
-    dp.add_handler(CommandHandler("start", start_command_handler))
-
-    # message handler
-    dp.add_handler(MessageHandler(Filters.text, main_handler))
-
-    # suggested_actions_handler
-    dp.add_handler(CallbackQueryHandler(main_handler, pass_chat_data=True, pass_user_data=True))
-
-
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    if DefaultConfig.MODE == 'webhook':
-
-        updater.start_webhook(listen="0.0.0.0",
-                              port=int(DefaultConfig.PORT),
-                              url_path=DefaultConfig.TELEGRAM_TOKEN)
-        updater.bot.setWebhook(DefaultConfig.WEBHOOK_URL + DefaultConfig.TELEGRAM_TOKEN)
-
-        logging.info(f"Start webhook mode on port {DefaultConfig.PORT}")
-    else:
-        updater.start_polling()
-        logging.info(f"Start polling mode")
-
-    updater.idle()
-
-
-class DefaultConfig:
-    PORT = int(os.environ.get("PORT", 3978))
-    TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
-    MODE = os.environ.get("MODE", "polling")
-    WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
-
-    LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO').upper()
-
-    @staticmethod
-    def init_logging():
-        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                            level=DefaultConfig.LOG_LEVEL)
-        #logging.config.fileConfig('logging.conf')
-
-
-if __name__ == '__main__':
-    ascii_banner = pyfiglet.figlet_format("AdvancedTelegramBot")
+if __name__ == "__main__":
+    ascii_banner = pyfiglet.figlet_format("PersonaForge TG Client")
     print(ascii_banner)
-
-    # Enable logging
-    DefaultConfig.init_logging()
-
     main()
